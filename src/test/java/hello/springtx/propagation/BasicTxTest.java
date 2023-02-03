784ae88291dfebb72ceedd2db271936d4f590cb1
Keunoh <rmsdhcjswo1@gmail.com>
2023-02-04 08:49:17 +0900

    Spring Transaction Thoery

--- src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -1,6 +1,7 @@
 package hello.springtx.propagation;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
 
 import javax.sql.DataSource;
@@ -16,6 +18,12 @@ import javax.sql.DataSource;
 @Slf4j
 @SpringBootTest
 public class BasicTxTest {
+
+    /**
+     * 대원칙
+     * 1. 모든 논러 트랜잭션이 커밋되어야 물리 트랜잭션이 커밋된다.
+     * 2. 하나의 논리 트랜잭션이라도 롤백되면 물리 트랜잭션은 롤백된다.
+     */
 
     @Autowired
     PlatformTransactionManager txManager;
@@ -104,5 +112,27 @@ public class BasicTxTest {
         txManager.rollback(outer);
     }
 
+    @Test
+    void inner_rollback() {
+        //논리 트랜잭션 중 하나라도 롤백 되면 해당 물리 트랜잭션은 롤백되어야 한다.
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
 
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("내부 트랜잭션 롤백");
+        txManager.rollback(inner); //rollback-only 표시
+
+        log.info("외부 트랜잭션 커밋");
+        //해당 런타임 익셉션을 날려서 확실하게 롤백된다는 것을 알려준다.
+        Assertions.assertThatThrownBy(() -> txManager.commit(outer))
+                .isInstanceOf(UnexpectedRollbackException.class);
+    }
+
+    /**
+     * 참고
+     * 애플리케이션 개발에서 중요한 기본 원칙은 모호함을 제거하는 것이다. 개발은 명확해야 한다.
+     * 이렇게 커밋을 호출했는데, 내부에서 롤백이 발생한 경우 모호하게 두면 아주 심각한 문제가 발생한다.
+     * 이렇게 기대한 결과가 다른 경우 예외를 발생시켜서 문제를 알려주는 것이 좋은 설계이다.
+     */
 }

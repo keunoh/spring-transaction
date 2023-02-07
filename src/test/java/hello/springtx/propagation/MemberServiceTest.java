45531325d4ab9aa9685cfaa37250956e4cfd7897
Keunoh <rmsdhcjswo1@gmail.com>
2023-02-07 23:35:45 +0900

    Transaction Propagation

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -124,6 +124,12 @@ class MemberServiceTest {
      */
     @Test
     void recoverException_fail() {
+        /**
+         * logRepository 에서 에러를 캐치하였더라도
+         * 물리 트랜잭션에서는 rollbackOnly=true 를 확인 했음으로
+         * 전부 롤백된다.
+         * 클라이언트로 UnexpectedRollbackException 을 반환한다.
+         */
         //given
         String username = "로그예외_recoverException_fail";
 
@@ -135,6 +141,31 @@ class MemberServiceTest {
         //then : 모든 데이터가 롤백된다.
         assertTrue(memberRepository.find(username).isEmpty());
         assertTrue(logRepository.find(username).isEmpty());
+    }
 
+    /**
+     * memberService    @Transactional:ON
+     * memberRepository @Transactional:ON
+     * logRepository    @Transactional:ON(REQUIRES_NEW) Exception
+     */
+    @Test
+    void recoverException_success() {
+        /**
+         * Propagation 옵션을 이용해서 트랜잭션을 분리한다
+         * 시나리오는 회원은 저장되고 로그는 롤백되어서 실패한다.
+         * 하나만 커밋되는 것이다.
+         * 단점은 이렇게 되면 2개의 데이터베이스 커넥션을 사용한다는 의미이고
+         * 이는 성능이 중요한 곳에서는 조심히 사용해야 한다는 의미다.
+         */
+
+        //given
+        String username = "로그예외_recoverException_success";
+
+        //when
+         memberService.joinV2(username);
+
+        //then : member 저장, log 롤백
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isEmpty());
     }
 }
